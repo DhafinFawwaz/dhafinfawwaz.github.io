ab7ec51b25de1855b4c2d13e9578259ebdf2442a
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-10-20 15:46:40 +0700

    feat: skill with text

--- components/skills.tsx
+++ components/skills.tsx
@@ -20,8 +20,9 @@ export default function SkillPage() {
             const currentSkill: Tag = tagsMap[skill];
             const tools = skillsMap[skill].map((tool: string, idx: number) => {   
                 const currentTool: Tag = tagsMap[tool];
-                return <a key={idx} href={"/tag?tag="+tool} className="w-5 max-h-5 self-center appearance-none hover:scale-125 active:scale-150 ease-out-back duration-150 z-20"><img src={currentTool.icon} alt={currentTool.name}/></a>;
-            });
+                return <a key={idx} href={"/tag?tag="+tool} className="text-2xs xs:text-xs font-semibold #ff5151 bg-night-600 rounded-lg pr-2 py-0.5 text-center z-40 hover:bg-indigo-600 active:bg-indigo-700 hover:cursor-pointer duration-75 shadow-rim-sm drop-shadow-sm focus:ring-4 ring-indigo-800 flex items-center gap-1"><img className="w-4 max-h-4 ml-1" src={currentTool.icon} alt={currentTool.name}/><span className="translate-y-[1px]">{currentTool.name}</span></a>;
+            });     
+            tools.push(<><div style={{flexGrow: 1000000}}></div></>)
             arr.push(
             <div key={key++} className="bg-night-900 drop-shadow-lg rounded-xl shadow-rim-sm ring-night-900 relative">
                 <Link href={"/tag?tag="+skill} className='w-full h-full hover:bg-night-800 active:bg-night-600 active:ring-4 ring-night-900 absolute rounded-xl p-4'>
@@ -32,7 +33,7 @@ export default function SkillPage() {
                     <div className="font-medium text-xs 4xs:text-sm sm:text-md mb-1 text-zinc-400 z-20 leading-5 -translate-y-1">Tools/Languages</div>
                 </Link>
                 <div className="p-4">
-                    <div className="flex gap-2 flex-wrap sm:mt-12 4xs:mt-12 mt-10">
+                    <div className="flex gap-1 flex-wrap sm:mt-12 4xs:mt-12 mt-10">
                         {tools}
                     </div>
                 </div>
