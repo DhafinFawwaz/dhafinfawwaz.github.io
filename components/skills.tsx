37a1d7195acee0cb33bec6b0af54097cedd452e9
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-01-31 16:56:28 +0700

    fix: safari flex grow & global text color

--- components/skills.tsx
+++ components/skills.tsx
@@ -27,7 +27,6 @@ export default function SkillPage() {
                 const currentTool: Tag = tagsMap[tool];
                 return <a key={idx} href={"/tag?tag="+tool} aria-label={"find "+currentSkill.name} className="text-2xs xs:text-xs font-semibold #ff5151 bg-night-600 rounded-lg pr-2 py-0.5 text-center z-40 hover:bg-indigo-600 active:bg-indigo-700 hover:cursor-pointer duration-75 shadow-rim-sm drop-shadow-sm focus:ring-4 ring-indigo-800 flex items-center gap-1"><img className="w-4 max-h-4 ml-1" src={currentTool.icon} alt={currentTool.name+" Icon"}/><span className="translate-y-[1px]">{currentTool.name}</span></a>;
             });     
-            tools.push(<div key={-1} style={{flexGrow: 1000000}}></div>)
             arr.push(
             <div key={key++} className={"bg-night-900 drop-shadow-lg rounded-xl shadow-rim-sm ring-night-900 relative" + applyPosition(key)}>
                 <Link href={"/tag?tag="+skill} aria-label={"find "+currentSkill.name} className='w-full h-full hover:bg-night-800 active:bg-night-600 active:ring-4 ring-night-900 absolute rounded-xl p-4'>
