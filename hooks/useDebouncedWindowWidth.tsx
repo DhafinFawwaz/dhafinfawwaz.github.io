2a01997f59814d149a700af16aaf2ccb4b911eac
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-02-16 16:31:57 +0700

    fix: resize event called when not resizing window

--- hooks/useDebouncedWindowWidth.tsx
+++ hooks/useDebouncedWindowWidth.tsx
@@ -1,26 +1,29 @@
 import { useEffect, useRef } from "react";
 
-function useDebouncedWindowWidth(callback: (width: number) => void, delay: number = 300) {
+export default function useDebouncedWindowWidth(callback: (width: number) => void, delay: number = 300) {
+  if(typeof window === "undefined") return;
   const timeoutRef = useRef<NodeJS.Timeout>();
+  const lastWidthRef = useRef(window.innerWidth);
 
   useEffect(() => {
-    function handleResize() {
+    const handleResize = () => {
+      if (window.innerWidth === lastWidthRef.current) return;
+
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
 
       timeoutRef.current = setTimeout(() => {
+        lastWidthRef.current = window.innerWidth;
         callback(window.innerWidth);
       }, delay);
     };
 
     window.addEventListener("resize", handleResize);
-    
-    handleResize();
 
     return () => {
       window.removeEventListener("resize", handleResize);
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
     };
   }, [callback, delay]);
+
 }
 
-export default useDebouncedWindowWidth;
